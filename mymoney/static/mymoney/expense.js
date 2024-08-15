0d44881cfba94c6ff90b64cea8487c8a05e14505
Franco-Postulka <francopostulka@hotmail.com>
2024-08-15 10:26:47 -0300

    Clean JS code

--- mymoney/static/mymoney/expense.js
+++ mymoney/static/mymoney/expense.js
@@ -24,17 +24,18 @@ function list_expenses(){
     .then(data => {
         try {
             if (data.length > 0) {
-                console.log(data);
                 data.forEach(expense => {
                     expense_div = document.createElement('div');
-                    expense_div.className = 'expense-div';
+                    expense_div.className = 'expense-div'; 
                     expense_div.setAttribute("id", `${expense.id}`);
 
+                    //Amount of the expense
                     amount_div = document.createElement('div');
                     amount_div.innerHTML = `$${expense.amount.toLocaleString()} `
                     amount_div.className = 'amount-div'
                     expense_div.append(amount_div);
-                    
+
+                    //Category of the expense
                     if (expense.category === null){
                         category_div = document.createElement('div');
                         category_div.innerHTML = 'No category'
@@ -45,6 +46,7 @@ function list_expenses(){
                     category_div.className = 'category-div'
                     expense_div.append(category_div);
 
+                    //Pyment method of the expense
                     if (expense.payment_method === null){
                         payment_div = document.createElement('div');
                         payment_div.innerHTML = 'No payment method'
@@ -55,41 +57,28 @@ function list_expenses(){
                     payment_div.className= 'payment-div'
                     expense_div.append(payment_div);
 
+                    //Date of the expense
                     date_div = document.createElement('div');
                     date_div.innerHTML = `${expense.date}`
                     date_div.className = 'date-div'
                     expense_div.append(date_div);
-                    
+
+                    //Expand button
                     expand_button = document.createElement('button');
                     expand_button.className = 'btn';
                     expand_icon = document.createElement('i');
                     expand_icon.className = "fa-solid fa-angles-down";
                     expand_button.append(expand_icon);
 
+                    //delete button
                     delete_button = document.createElement('button');
                     delete_button.className = 'btn';
-                    delete_button.onclick = function(){
-                        // console.log(this.parentElement.parentElement.id);
-                        const csrftoken = getCSRFToken();
-                        fetch(`remove/${this.parentElement.parentElement.id}`,{
-                            method: 'DELETE',
-                            headers: {
-                                'X-CSRFToken': csrftoken,  // include the CSRF token
-                            },
-                        })
-                        .then(response => {
-                            if(response.ok){
-                                this.parentElement.parentElement.style.animationPlayState = 'running';
-                                this.parentElement.parentElement.addEventListener('animationend', () => {
-                                    this.parentElement.parentElement.remove();
-                                });
-                            }
-                        })
-                    };
+                    delete_button.onclick = delete_function;
                     delete_icon = document.createElement('i');
                     delete_icon.className = "fa-solid fa-trash-can";
                     delete_button.append(delete_icon);
-
+                    
+                    //Icons div
                     icons_div = document.createElement('div');
                     icons_div.append(expand_button);
                     icons_div.append(delete_button);
@@ -107,6 +96,23 @@ function list_expenses(){
     })
 }
 
+function delete_function(){
+    const csrftoken = getCSRFToken();
+    fetch(`remove/${this.parentElement.parentElement.id}`,{
+        method: 'DELETE',
+        headers: {
+            'X-CSRFToken': csrftoken,  // include the CSRF token
+        },
+    })
+    .then(response => {
+        if(response.ok){
+            this.parentElement.parentElement.style.animationPlayState = 'running';
+            this.parentElement.parentElement.addEventListener('animationend', () => {
+                this.parentElement.parentElement.remove();
+            });
+        }
+    })
+}
 function getCSRFToken() {
     let cookieValue = null;
     const cookies = document.cookie.split(';');
